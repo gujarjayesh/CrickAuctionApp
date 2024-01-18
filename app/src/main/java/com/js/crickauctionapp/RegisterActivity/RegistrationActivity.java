ad60a5a916c39c80e16aa93004db0373e0021fd2
AmitP <vu4f2122021@pvppcoe.ac.in>
2024-01-18 10:59:15 +0530

    splash3

---
+++ app/src/main/java/com/js/crickauctionapp/RegisterActivity/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.js.crickauctionapp.RegisterActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.icspl.crickauctionapp.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
--- app/src/main/java/com/js/crickauctionapp/SplashActivity.java
+++ app/src/main/java/com/js/crickauctionapp/SplashActivity.java
@@ -2,15 +2,56 @@ package com.js.crickauctionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.icspl.crickauctionapp.R;
+import com.js.crickauctionapp.RegisterActivity.RegistrationActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView autionSplashImg;
+    TextView splashCrikAutionTxt;
+    ObjectAnimator animator;
+    LottieAnimationView splashHammer;
+    private static final int SPLASH_TIMEOUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+
+        autionSplashImg = findViewById(R.id.autionSplashImg);
+        splashCrikAutionTxt = findViewById(R.id.splashCrikAutionTxt);
+        splashHammer = findViewById(R.id.splashHammer);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the main activity after the splash timeout
+                Intent intent = new Intent(SplashActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        animator = ObjectAnimator.ofFloat(splashCrikAutionTxt, "translationY", -1050f);
+        animator.setDuration(2000);
+        animator.start();
+        splashHammer.animate().getStartDelay();
     }
 }
